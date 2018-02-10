c7bae0f25f5715c57a7007df5506487ffa5d3f5d
Michal Nowakowski <michal@nowakowski.me.uk>
2018-02-10 19:34:57 +0100

    client base

--- states-client/src/main/java/StatesService.java
+++ states-client/src/main/java/StatesService.java
@@ -1,3 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
 public class StatesService {
 
+    public static final String ADDRESS = "http://services.groupkt.com/state/get/USA/all";
+
+    public List getAllStates() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(ADDRESS);
+        Response response = target.request().get();
+
+        //
+        String result = response.readEntity(String.class);
+        System.out.println(result);
+
+        return new ArrayList();
+    }
 }
--- states-client/src/test/java/StatesServiceTest.java
+++ states-client/src/test/java/StatesServiceTest.java
@@ -1,3 +1,17 @@
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+
+import java.util.List;
+import org.junit.Test;
+
 public class StatesServiceTest {
 
+    @Test
+    public void shouldGetAllStates() {
+        StatesService service = new StatesService();
+
+        List states = service.getAllStates();
+        assertThat(states.size(), is(55));
+    }
 }
