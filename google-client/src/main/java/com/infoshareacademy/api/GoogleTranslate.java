eba2ff6367982e712f2100ca7b2fa640c0799c9d
Michal Nowakowski <michal@nowakowski.me.uk>
2018-02-10 16:50:02 +0100

    generate url

--- google-client/src/main/java/com/infoshareacademy/api/GoogleTranslate.java
+++ google-client/src/main/java/com/infoshareacademy/api/GoogleTranslate.java
@@ -1,5 +1,12 @@
 package com.infoshareacademy.api;
 
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
 public class GoogleTranslate {
 
     private final String API_KEY;
@@ -8,7 +15,29 @@ public class GoogleTranslate {
         this.API_KEY = API_KEY;
     }
 
+    public String generateUrl(String address, Map<String, String> params) {
+        return "";
+    }
+
     public String translate(String input, String source, String target) {
-        throw new UnsupportedOperationException("Not implemented yet");
+
+        String address = "https://translation.googleapis.com/language/translate/v2";
+
+        Map<String, String> params = new HashMap<>();
+        params.put("source", source);
+        params.put("target", target);
+        params.put("key", API_KEY);
+        params.put("q", input);
+
+        String url = generateUrl(address, params);
+
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target(url);
+        Response response = webTarget.request().get();
+
+        String result = response.readEntity(String.class);
+        response.close();
+
+        return result;
     }
 }
--- google-client/src/test/java/com/infoshareacademy/api/GoogleTranslateTest.java
+++ google-client/src/test/java/com/infoshareacademy/api/GoogleTranslateTest.java
@@ -3,12 +3,14 @@ package com.infoshareacademy.api;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.Before;
 import org.junit.Test;
 
 public class GoogleTranslateTest {
 
-    public static final String API_KEY = "";
+    public static final String API_KEY = "AIzaSyBFhP9kVN4d_hYuD2tz6yzri6cHYH6tAe0";
 
     private GoogleTranslate googleTranslate;
 
@@ -25,5 +27,18 @@ public class GoogleTranslateTest {
 
         final String result = googleTranslate.translate(input, source, target);
         assertThat(result, is("Cześć jak się masz?"));
+    }
+
+    @Test
+    public void shouldGenerateUrl() {
+        String address = "http://localhost/";
+        Map<String, String> params = new HashMap<>();
+        params.put("param1", "1");
+        params.put("param2", "abc");
+
+        String expectedUrl = "http://localhost/?param1=1&param2=abc";
+
+        String url = googleTranslate.generateUrl(address, params);
+        assertThat(url, is(expectedUrl));
     }
 }
