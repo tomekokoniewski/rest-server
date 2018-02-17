0dfdbdb869f609ce742be7be5f1488fe5471cfe7
MichalNowakowski <michal@nowakowski.me.uk>
2018-02-17 16:30:54 +0100

    Cwiczenie 2

--- rest-server/src/main/java/com/infoshareacademy/service/UserService.java
+++ rest-server/src/main/java/com/infoshareacademy/service/UserService.java
@@ -7,13 +7,18 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/")
 public class UserService {
 
     private Logger LOG = LoggerFactory.getLogger(UserService.class);
+
+    @Context
+    private UriInfo uriInfo;
 
     public UserService() {
     }
@@ -23,6 +28,10 @@ public class UserService {
     @Produces(MediaType.TEXT_PLAIN)
     public Response sayHello(@PathParam("name") String name) {
         LOG.info("Saying hello to {}!", name);
+
+        LOG.info("Absolute Path: {}", uriInfo.getAbsolutePath());
+        LOG.info("Base URI: {}", uriInfo.getBaseUri());
+        LOG.info("Path Parameters: {}", uriInfo.getPathParameters());
 
         return Response.ok("Saying hello to " + name + "!").build();
     }
